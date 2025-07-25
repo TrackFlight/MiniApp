a1a2aaf13453ce48a3b566ced2c89cba569aaaaf
Laky64 <iraci.matteo@gmail.com>
2025-07-25 10:42:59 +0200

    Remove Old Deprecated Bulletin Code

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,20 @@
 <script lang="ts">
-    import {telegram, type BulletinButton} from "./lib/telegram";
-    import { fade } from 'svelte/transition';
+    import {telegram} from "./lib/telegram";
+    import {fade} from 'svelte/transition';
     import Home from "./pages/Home.svelte";
     import Navbar from "./components/Navbar.svelte";
-    import { tryLogin } from "./lib/api";
+    import {tryLogin} from "./lib/api";
     import LoadingDialog from "./components/LoadingDialog.svelte";
     import Bulletin from "./components/Bulletin.svelte";
-    import { bulletinState } from "./lib/stores";
 
     telegram?.ready();
     telegram?.setHeaderColor("secondary_bg_color");
     telegram?.setBackgroundColor("secondary_bg_color");
     telegram.showLoadingProgress = (show: boolean) => loading = show;
     telegram.showPrompt = (text: string, callback: (result: string | null) => void) => callback(prompt(text));
-    telegram.showBulletin = (icon: string, text: string, duration?: number, title?: string, button?: BulletinButton, on_close?: () => void) => {
-        bulletinState.update((current) => {
-            const isSame = current?.text === text && current?.icon === icon && current?.title === title && current?.icon !== 'timer';
-            return {
-                title: title,
-                text: text,
-                icon: icon,
-                duration: duration || 1800,
-                button: button,
-                on_close: on_close,
-                shake: isSame,
-                reopen: bulletinEl != undefined && !isSame,
-            };
-        });
-        bulletin = true;
-    };
 
-    let bulletin = $state(false);
     let logged = $state(false);
     let loading = $state(false);
-    let bulletinEl: Bulletin | undefined = $state();
 
     tryLogin().then((success: boolean) => {
         if (success) {
@@ -56,9 +37,7 @@
             <div>
                 <Home/>
             </div>
-            {#if bulletin}
-                <Bulletin bind:this={bulletinEl} on_close={() => bulletin = false}/>
-            {/if}
+            <Bulletin/>
         </div>
         <Navbar/>
         {#if loading}
