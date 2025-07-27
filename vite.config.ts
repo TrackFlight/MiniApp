400d186b178bc46dcf60e63106c5d85e1d42efb5
Laky64 <iraci.matteo@gmail.com>
2025-07-27 15:16:31 +0200

    Optimized Vite Vendoring

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+const SVELTE_VENDOR = [
+    'clsx',
+    'esm-env',
+    'svelte',
+];
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -27,7 +33,11 @@ export default defineConfig({
     rollupOptions: {
       output:{
         manualChunks(id) {
-          if (id.includes('node_modules')) {
+          const isSvelteVendor = SVELTE_VENDOR
+              .some(vendor => id.includes(`node_modules/${vendor}/`));
+          if (isSvelteVendor) {
+            return 'svelte-vendor';
+          } else if (id.includes('node_modules')) {
             return id.toString().split('node_modules/')[1].split('/')[0].toString();
           }
         }
