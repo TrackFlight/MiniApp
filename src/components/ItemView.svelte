8ac1e30f631ebe144df779b9ac2bb0ffa092c0ef
Laky64 <iraci.matteo@gmail.com>
2025-07-27 10:38:48 +0200

    Fixed ItemView Click when Deleting

--- src/components/ItemView.svelte
+++ src/components/ItemView.svelte
@@ -31,7 +31,7 @@
 </script>
 
 <div class="itemView" class:deletable class:isiOS class:isDesktop>
-    <Button on_click={() => {if (on_click) on_click()}}>
+    <Button on_click={() => {if (on_click && (!deletable || !isiOS)) on_click()}}>
         {#if icon === "add"}
             <svg class="icon" width="28px" height="28px" fill="var(--tg-theme-accent-text-color)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22.627 18.252">
                 <g>
