205a69be2b60a45752c47847d264899f1cbf4abc
Laky64 <iraci.matteo@gmail.com>
2025-08-03 18:44:15 +0200

    Fixed Desktop OS Detection

--- src/lib/telegram.ts
+++ src/lib/telegram.ts
@@ -65,5 +65,5 @@ interface TelegramWindow extends Window {
 
 export const telegram = (window as unknown as TelegramWindow).Telegram!.WebApp
 export const currentUser = telegram!.initDataUnsafe.user;
-export const isDesktop = (telegram.platform === 'tdesktop' || telegram.platform === 'macos' || telegram.platform.startsWith('web')) && !('ontouchstart' in window);
+export const isDesktop = telegram.platform === 'tdesktop' || telegram.platform === 'macos' || (telegram.platform.startsWith('web') && !('ontouchstart' in window));
 export const isiOS = telegram.platform === 'ios' || telegram.platform === 'macos';
