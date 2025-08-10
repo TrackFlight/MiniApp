dffaafff2d3e4002deb985dea852511b986eacd8
Laky64 <iraci.matteo@gmail.com>
2025-08-10 19:21:51 +0200

    Adds scrollable page component

---
+++ src/components/ScrollablePage.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    const {children} : {children: any} = $props();
+</script>
+
+<div class="content">
+    {@render children?.()}
+</div>
+
+<style>
+    .content {
+        height: 100%;
+        overflow-y: auto;
+        padding-bottom: 10px;
+    }
+
+    .content::-webkit-scrollbar {
+        display: none;
+    }
+</style>
